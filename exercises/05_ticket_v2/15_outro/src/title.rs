a2eb08a933438438cb418d41d0c846ed5c5488cf
JD Singh <info@fermatsolutions.dev>
2024-10-31 11:17:27 -0500

    Saving work

--- exercises/05_ticket_v2/15_outro/src/title.rs
+++ exercises/05_ticket_v2/15_outro/src/title.rs
@@ -2,8 +2,22 @@
 //   enforcing that the title is not empty and is not longer than 50 bytes.
 //   Implement the traits required to make the tests pass too.
 
+#[derive(Debug, PartialEq, Clone)]
 pub struct TicketTitle(String);
 
+#[derive(Debug, thiserror::Error)]
+#[error("{invalid_title}")]
+struct ParseTicketTitle {
+    invalid_title: String,
+}
+
+impl TryFrom<&str> for TicketTitle {
+    type Error = ParseTicketTitle;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
