e9fa1949d759f6a8cce1f8fd55d667ec21d71e98
JD Singh <info@fermatsolutions.dev>
2024-11-11 13:04:06 -0600

    Saving work

--- exercises/06_ticket_management/14_index_mut/src/lib.rs
+++ exercises/06_ticket_management/14_index_mut/src/lib.rs
@@ -1,6 +1,6 @@
 // TODO: Implement `IndexMut<&TicketId>` and `IndexMut<TicketId>` for `TicketStore`.
 
-use std::ops::Index;
+use std::ops::{Index, IndexMut};
 use ticket_fields::{TicketDescription, TicketTitle};
 
 #[derive(Clone)]
@@ -75,6 +75,20 @@ impl Index<&TicketId> for TicketStore {
     }
 }
 
+impl IndexMut<TicketId> for TicketStore {
+    fn index_mut(&mut self, index: TicketId) -> &mut Self::Output {
+        self.tickets
+            .iter_mut()
+            .find(|ticket| ticket.id == index)
+            .unwrap()
+    }
+}
+
+impl IndexMut<&TicketId> for TicketStore {
+    fn index_mut(&mut self, index: &TicketId) -> &mut Self::Output {
+        &mut self[*index]
+    }
+}
 #[cfg(test)]
 mod tests {
     use crate::{Status, TicketDraft, TicketStore};
